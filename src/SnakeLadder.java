10e2754749f23cc0a44a53b32de0bfbe25075d37
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-01 10:01:53 +0530

    Added: Check for a option. No Play, Ladder or Snake

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -2,13 +2,44 @@
 public class SnakeLadder {
 
 	final static int startPosition=0;
-    final static int NumberOfPlayer=1;
+	final static int NumberOfPlayer=1;
+	final static int noPlay=1;
+	final static int ladder=2;
+	final static int snake=3;
+	static int userPosition = startPosition;
+	
+	public static int getUserPosition() {
+		return userPosition;
+	}
+	
+	public static void setUserPosition(int userPosition) {
+		SnakeLadder.userPosition = userPosition;
+	}
     
     public static int rollDice() {
     	int diceValue = (int)(Math.floor(Math.random()*10)%6)+1;
     	return diceValue;
     }
-	public static void main(String[] args) {
-			System.out.println(rollDice());
+    
+    public static int playerOption() {
+    	int option = (int)(Math.floor(Math.random()*10)%3)+1;
+    	return option;
+    }
+    
+    public static void main(String[] args) {
+		int diceNumber = rollDice();
+		int option = playerOption();
+		
+		if(option == noPlay) {
+			setUserPosition(getUserPosition());
+		}
+		else if(option == ladder) {
+			setUserPosition(getUserPosition() + diceNumber);
+		}
+		else if(option == snake) {
+			setUserPosition(getUserPosition() - diceNumber);
+		}
+		
+		System.out.println(getUserPosition());
 	}
 }
