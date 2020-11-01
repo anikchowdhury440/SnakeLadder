d45dbc3154f97430fecad2b9e8ffd6068b18d754
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-01 09:40:01 +0530

    Added: Random Dice Roll

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -3,7 +3,12 @@ public class SnakeLadder {
 
 	final static int startPosition=0;
     final static int NumberOfPlayer=1;
+    
+    public static int rollDice() {
+    	int diceValue = (int)(Math.floor(Math.random()*10)%6)+1;
+    	return diceValue;
+    }
 	public static void main(String[] args) {
-			
+			System.out.println(rollDice());
 	}
 }
